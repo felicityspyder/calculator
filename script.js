174e1054d2b8effaf188ebe9bb9ca5b0cfeb73fc
josiah <felicityspyder@protonmail.comm>
2023-05-13 11:19:08 -0400

    Create function for each arithmetic operator

---
+++ script.js
@@ -0,0 +1,39 @@
+const add = function(a, b) {
+    return a + b;	
+  };
+  
+  const subtract = function(a, b) {
+      return a - b;
+  };
+  
+  const sum = function(a) {
+    let total = 0;
+    for (let i = 0; i < a.length; i++) {
+      total += a[i];
+    };
+    return total;
+  };
+  
+  const multiply = function(a) {
+    let total = 1;
+    for (let i = 0; i < a.length; i++) {
+      total = total * a[i];
+    }
+    return total;
+  };
+
+  const divide = function(a, b) {
+    return a / b;
+  }
+  
+  const power = function(a, b) {
+    return a ** b;
+  };
+  
+  const factorial = function(a) {
+      let total = 1;
+    for (let i = 2; i <= a; i++) {
+      total *= i;
+    }
+    return total;
+  };
