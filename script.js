bedaf6b505725fc1eaa2a39cfe08318817fe6bb7
josiah <felicityspyder@protonmail.comm>
2023-05-13 12:30:03 -0400

    Create operate function that calls 2 numbers and operator

--- script.js
+++ script.js
@@ -6,34 +6,56 @@ const add = function(a, b) {
       return a - b;
   };
   
-  const sum = function(a) {
-    let total = 0;
-    for (let i = 0; i < a.length; i++) {
-      total += a[i];
-    };
-    return total;
-  };
+const sum = function(a) {
+let total = 0;
+for (let i = 0; i < a.length; i++) {
+    total += a[i];
+};
+return total;
+};
   
-  const multiply = function(a) {
-    let total = 1;
-    for (let i = 0; i < a.length; i++) {
-      total = total * a[i];
-    }
-    return total;
-  };
+const multiply = function(a) {
+let total = 1;
+for (let i = 0; i < a.length; i++) {
+    total = total * a[i];
+}
+return total;
+};
 
-  const divide = function(a, b) {
-    return a / b;
-  }
+const divide = function(a, b) {
+return a / b;
+}
   
-  const power = function(a, b) {
-    return a ** b;
-  };
+const power = function(a, b) {
+return a ** b;
+};
   
-  const factorial = function(a) {
-      let total = 1;
-    for (let i = 2; i <= a; i++) {
-      total *= i;
-    }
-    return total;
-  };
+const factorial = function(a) {
+    let total = 1;
+for (let i = 2; i <= a; i++) {
+    total *= i;
+}
+return total;
+};
+
+let a; 
+let operator;
+let b
+
+function operate() {
+    switch(operator) {
+        case '+':  
+            add(a, b);
+            break;
+        case '-':
+            subtract(a, b);
+            break;
+        case '*':
+            multiply(a, b);
+            break;
+        case "/":
+            divide(a, b);
+            break;
+    } 
+}
+
