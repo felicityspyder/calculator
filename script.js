cd83db195ccc580bb676e08254ae7422c7fd7304
josiah <felicityspyder@protonmail.comm>
2023-05-14 13:36:02 -0400

    Rewrote variables for numbers, operators, operands

--- script.js
+++ script.js
@@ -3,74 +3,6 @@ const container = document.createElement('div');
 container.classList.add("container");
 body.appendChild(container);
 
-const numericPad = function(rows, columns) {
-
-container.innerHTML = '';
-container.style.display = 'flex';
-container.style.flexDirection = 'column';
-
-    for (let i = 3; i > 0; i--) {
-        const row = document.createElement('div');
-        row.classList.add('numeral-rows'[i]);
-        container.appendChild(row);
-
-        let columns = 3;
-        for (let j = 0; j < columns; j++) {
-            const numeral = document.createElement('button');
-            numeral.classList.add('num');
-            numeral.textContent = numeral.id = (i * columns) + (j - 2);
-            row.appendChild(numeral);
-        }
-    }
-
-
-    for (let i = 1; i > 0; i--) {
-        const row = document.createElement('div');
-        row.classList.add('decimal-zero-equals');
-        container.appendChild(row);
-        
-        const decimal = document.createElement('button');
-        decimal.textContent = decimal.id = ".";
-        row.appendChild(decimal);
-
-        const zero = document.createElement('button');
-        zero.textContent = zero.id = "0"
-        row.appendChild(zero);
-
-        const equals = document.createElement('button');
-        equals.textContent = equals.id = "=";
-        row.appendChild(equals);
-    }
-
-
-    const operatorColumn = document.createElement('div');
-    operatorColumn.classList.add('operator-column');
-    body.appendChild(operatorColumn);
-    operatorColumn.innerHTML = '';
-    operatorColumn.style.display = 'flex';
-    operatorColumn.style.flexDirection = 'column';
-    
-    const divide = document.createElement('button');
-    divide.textContent = divide.id = "/";
-    operatorColumn.appendChild(divide);
-
-    const multiply = document.createElement('button');
-    multiply.textContent = multiply.id = "*";
-    operatorColumn.appendChild(multiply);
-
-    const subtract = document.createElement('button');
-    subtract.textContent = subtract.id = "-";
-    operatorColumn.appendChild(subtract);
-
-    const plus = document.createElement('button');
-    plus.textContent = plus.id = "+";
-    operatorColumn.appendChild(plus);
-  
-    }
-
-
-numericPad();
-
 const add = function(a, b) {
     return a + b;	
   };
@@ -111,9 +43,13 @@ for (let i = 2; i <= a; i++) {
 return total;
 };
 
-let a; 
-let operator = ["=", "-", "*", "/",];
-let b;
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const equalsButton = document.querySelector('[data=equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
 
 
 function operate(a, b, operator) {
@@ -127,6 +63,23 @@ function operate(a, b, operator) {
         case "/":
             return divide(a, b);
     } 
+}
+// populate display when clicking number buttons
+function currentValue(e) {
+const value = document.querySelector('button');
+console.log(value);
+}
+
+currentValue();
+
+// const previousValue = document.querySelector(`button[data-key=${e.keycode}]`);
+
+function currentOperand () {
+
+}
+
+function previousOperand () {
+
 }
 
 console.log(operate())
